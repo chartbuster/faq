91fadb6a2fac380a7873f5272bd1bccdc572705e
hari cheboyina <hc373@njit.edu>
2018-04-29 20:24:10 -0400

    Created question form to add a question and save it!

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -29,7 +29,11 @@ class QuestionController extends Controller
      */
     public function create()
     {
-        //
+
+        $question = new Question;
+        $edit = FALSE;
+        return view('questionForm', ['question' => $question,'edit' => $edit  ]);
+
     }
 
     /**
@@ -40,8 +44,28 @@ class QuestionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $input = $request->validate([
+            'body' => 'required|min:5',
+        ], [
+
+            'body.required' => 'Body is required',
+            'body.min' => 'Body must be at least 5 characters',
+
+        ]);
+        $input = request()->all();
+
+        $question = new Question($input);
+        $question->user()->associate(Auth::user());
+        $question->save();
+
+        return redirect()->route('home')->with('message', 'IT WORKS!');
+
+
+
+        // return redirect()->route('questions.show', ['id' => $question->id]);
+
     }
+
 
     /**
      * Display the specified resource.
